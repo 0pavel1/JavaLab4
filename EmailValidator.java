e29c59fd361bf717dcb21a4441c2f4a338ad05ad
Павел Владимирович Заморский <122034215+0pavel1@users.noreply.github.com>
2024-10-23 19:01:01 +0300

    update file

--- EmailValidator.java
+++ EmailValidator.java
@@ -1,3 +1,7 @@
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
 class CustomEmailFormatException extends Exception {
     public CustomEmailFormatException(String message) {
         super(message);
@@ -6,12 +10,13 @@ class CustomEmailFormatException extends Exception {
 
 public class EmailValidator {
     public static void main(String[] args) {
-        String email = "pavel.zamorskiy@mail.ru";
+        String email = "pavel.zamorskiy@";
         
         try {
             validateEmail(email);
         } catch (CustomEmailFormatException e) {
             System.out.println("Ошибка: " + e.getMessage());
+            logErrorToFile(e.getMessage());
         }
     }
     
@@ -19,13 +24,24 @@ public class EmailValidator {
         if (!email.contains("@")) {
             throw new CustomEmailFormatException("Некорректный формат email-адреса. ВЫ ПРОПУСТИЛИ @");
         }
-        else if (!email.matches(".+\\.[a-zA-Z]{2,}$")){
+        else if (!email.matches(".+\\.[a-zA-Z]{2,}$")) {
             throw new CustomEmailFormatException("Некорректный формат email-адреса. ВЫ НЕ УКАЗАЛИ ИЛИ НЕПРАВИЛЬНО УКАЗАЛИ ДОМЕННОЕ ИМЯ");
         }
-        else if (!email.matches("^.+@*")){
+        else if (!email.matches("^.+@*")) {
             throw new CustomEmailFormatException("Некорректный формат email-адреса. ВЫ НЕ УКАЗАЛИ ПОЛЬЗОВАТЕЛЯ");
         }
-        else
+        else {
             System.out.println("Email - " + email + " корректен.");
+        }
+    }
+
+    public static void logErrorToFile(String errorMessage) {
+        String logMessage = "Ошибка: " + errorMessage + "\n";
+        try (FileOutputStream fos = new FileOutputStream("errors.log", true)) {
+            fos.write(logMessage.getBytes(StandardCharsets.UTF_8));
+        } catch (IOException e) {
+            System.out.println("Не удалось записать ошибку в файл: " + e.getMessage());
+        }
     }
 }
+
